6f8c55ba7d69506bfdd0d5b902e4a82971eb7505
Kacper Rabczewski <krabczewski@gmail.com>
2024-05-27 21:49:59 +0200

    final touches

--- RunningApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RunningApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -118,7 +118,7 @@ namespace RunningApp.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Dashboard");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
